4b5474e8f2d587b2fd5efe66f6ed4089d4735120
Waheed Ahmad <a@b.c>
2020-07-23 09:05:44 +0500

    add swagger

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,19 @@ namespace WebApi
 
             // configure DI for application services
             services.AddScoped<IUserService, UserService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Weather Data",
+                    Version = "v1",
+                    // You can also set Description, Contact, License, TOS...
+                });
+
+                // Configure Swagger to use the xml documentation file
+                //var xmlFile = Path.ChangeExtension(typeof(Startup).Assembly.Location, ".xml");
+                //c.IncludeXmlComments(xmlFile);
+            });
         }
 
         // configure the HTTP request pipeline
@@ -40,10 +53,20 @@ namespace WebApi
                 .AllowAnyMethod()
                 .AllowAnyHeader());
 
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             // custom jwt auth middleware
             app.UseMiddleware<JwtMiddleware>();
 
             app.UseEndpoints(x => x.MapControllers());
+
+            
         }
     }
 }
